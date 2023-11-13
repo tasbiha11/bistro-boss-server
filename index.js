8b901d57895b80ad4360273bf0fdd59e1778802e
tasbiha11 <tasbihahussain@yahoo.com>
2023-11-13 22:39:22 +0600

    manage items

--- index.js
+++ index.js
@@ -130,6 +130,20 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem)
+            res.send(result);
+        })
+
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
         //review related apis
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
