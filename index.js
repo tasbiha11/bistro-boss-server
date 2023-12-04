50ae05e60d916e268b861c963f3f2ab5a6f7e205
tasbiha11 <tasbihahussain@yahoo.com>
2023-12-04 22:43:28 +0600

    mozilla-vercel

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(cors());
 app.use(express.json());
 
