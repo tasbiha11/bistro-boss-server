22ba9bd144cc5c57ffc7381b6e1a5611297598ae
tasbiha11 <tasbihahussain@yahoo.com>
2023-12-04 22:15:34 +0600

    config

--- index.js
+++ index.js
@@ -8,15 +8,8 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    app.use(express.json());
-    next();
-});
-
+app.use(cors());
+app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
